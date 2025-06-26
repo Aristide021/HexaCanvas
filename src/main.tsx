ce49a5812ad59236a6d3086ee8c4221613005578
Sheldon Aristide <44308312+Aristide021@users.noreply.github.com>
2025-06-26 12:10:59 -0400

    Fix Immer MapSet plugin error

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { enableMapSet } from 'immer';
 import App from './App.tsx';
 import './index.css';
+
+// Enable Immer MapSet plugin for handling Map and Set objects
+enableMapSet();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
